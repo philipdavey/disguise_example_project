9b62ce95280144578e8e1711709818d1e94e02d1
Philip Davey <97410670+philipdavey@users.noreply.github.com>
2024-08-08 15:50:51 +0100

    Added Clock Wizard

---
+++ source/ip_cores/clk_wiz_0/clk_wiz_0_stub.vhdl
@@ -0,0 +1,30 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+-- Date        : Thu Aug  8 10:44:41 2024
+-- Host        : GBR-L-8PQ9JR3 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub c:/GitHub/example_project/source/ip_cores/clk_wiz_0/clk_wiz_0_stub.vhdl
+-- Design      : clk_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xcku5p-ffvd900-2L-e
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wiz_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    resetn : in STD_LOGIC;
+    clk_in1_p : in STD_LOGIC;
+    clk_in1_n : in STD_LOGIC
+  );
+
+end clk_wiz_0;
+
+architecture stub of clk_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,resetn,clk_in1_p,clk_in1_n";
+begin
+end;
